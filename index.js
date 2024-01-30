6afdd4b75bea3f177fb4da57dccf523338f8fa82
Mindy D Tran <mindytran98@gmail.com>
2024-01-30 11:01:50 -0500

    create /api/movies route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "./loadEnv.js"
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import moviesRouter from "./routes/movies.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,6 +12,9 @@ app.use(cors()); //allows fromedn to connect to backend
 app.use(morgan("dev")); //logger
 app.use(express.json()); // for data in req.body
 app.use(express.urlencoded({ extended: true })); //allow data in url string
+
+// routes
+app.use('/api/movies', moviesRouter);
 
 app.get("/", (req, res) => {
   res.send("hewo this is the backend");
