da5bddc9d2abc9fe8aac4b4626f903a51fa75f58
Mindy D Tran <mindytran98@gmail.com>
2024-01-30 10:01:44 -0500

    add middlewares

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
-const PORT =  process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-app.get("/", (req,res)=>{
-    res.send("hewo this is the backend")
+// middlewares
+app.use(cors()); //allows fromedn to connect to backend
+app.use(morgan("dev")); //logger
+app.use(express.json()); // for data in req.body
+app.use(express.urlencoded({ extended: true })); //allow data in url string
+
+app.get("/", (req, res) => {
+  res.send("hewo this is the backend");
 });
 
-app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
