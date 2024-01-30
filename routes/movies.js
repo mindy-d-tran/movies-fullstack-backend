98a7a104aafe0170ed649c138e8a96b91f3299ea
Mindy D Tran <mindytran98@gmail.com>
2024-01-30 11:11:01 -0500

    add basic get for api movies route

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import db from "../db/conn.js";
 
 const router = new Router();
 
-router.get("/",  (req, res) => {
-  res.send("movies...");
+router.get("/", async (req, res) => {
+  const collection = await db.collection("movies");
+
+  const movies = await collection.find().toArray();
+  const topTen = movies.slice(0,10);
+
+  res.send(topTen);
 });
 
 export default router;
