93a9a084c42c0a9d39e48d254d2c635c96a54b3e
Mindy D Tran <mindytran98@gmail.com>
2024-01-30 11:24:39 -0500

    add tryy and catch

--- routes/movies.js
+++ routes/movies.js
@@ -4,12 +4,16 @@ import db from "../db/conn.js";
 const router = new Router();
 
 router.get("/", async (req, res) => {
-  const collection = await db.collection("movies");
+  try {
+    const collection = await db.collection("movies");
 
-  const movies = await collection.find().toArray();
-  const topTen = movies.slice(0,10);
+    const movies = await collection.find().toArray();
+    const topTen = movies.slice(0, 10);
 
-  res.send(topTen);
+    res.status(200).send(topTen);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export default router;
