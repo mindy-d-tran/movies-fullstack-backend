bb18d45db9a027300886b35fca153e8fb44d03ce
Mindy D Tran <mindytran98@gmail.com>
2024-01-30 09:49:42 -0500

    add boilerplate for backend to start

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import express from "express";
+
+const app = express();
+const PORT =  process.env.PORT || 4000;
+
+app.get("/", (req,res)=>{
+    res.send("hewo this is the backend")
+});
+
+app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
