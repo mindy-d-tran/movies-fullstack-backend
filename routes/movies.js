e0d147c4dedf316901e5d8ef8c3ae3ddfe221dfa
Mindy D Tran <mindytran98@gmail.com>
2024-01-31 13:58:14 -0500

    add route for :/id

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { ObjectId } from "mongodb";
 import db from "../db/conn.js";
 
 const router = new Router();
@@ -11,6 +12,23 @@ router.get("/", async (req, res) => {
     const topTen = movies.slice(0, 10);
 
     res.status(200).send(topTen);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * GET /:id
+ */
+router.get("/:id", async (req, res) => {
+  try {
+    const collection = await db.collection("movies");
+
+    const query = new ObjectId(req.params.id)
+
+    const movie = await collection.findOne({ _id: query });
+
+    res.status(200).json(movie);
   } catch (error) {
     console.log(error);
   }
